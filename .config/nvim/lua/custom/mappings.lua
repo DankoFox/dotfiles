099609d47f8d2c0adce22f3d8aefe8b47049f6c5
Danko Fox <79992320+DankChoir@users.noreply.github.com>
2023-06-17 16:53:41 +0700

    Update mappings.lua

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -6,6 +6,12 @@ M.general = {
     ["<C-Y>"] = {"<C-R>","redo"},
     ["<C-C>"] = {'"+y',"copy"},
     ["<C-V>"] = {'"+p',"paste"},
+    ["gpd"] = { "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", "Preview Def" },
+    ["gpt"] = { "<cmd>lua require('goto-preview').goto_preview_type_definition()<CR>", "Preview Type" },
+    ["gpi"] = { "<cmd>lua require('goto-preview').goto_preview_implementation()<CR>", "Preview Imp" },
+    ["gP"] = { "<cmd>lua require('goto-preview').close_all_win()<CR>", "Close all preview windows" },
+    ["gpr"] = { "<cmd>lua require('goto-preview').goto_preview_references()<CR>", "Preview ref" },
+
   },
   v = {
     ["<C-C>"] = {'"+y',"copy"},
