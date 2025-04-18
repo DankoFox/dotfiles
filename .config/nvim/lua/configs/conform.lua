e60f2cbd63a96b4ae73c34c0bd162d52910272b6
DankChoir <timzek7@gmail.com>
2025-04-18 22:59:10 +0700

    [UPDATE-NEOVIM]

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,7 +1,7 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    python = { "black" },
+    python = { "ruff_format" },
     cpp = { "clang_format" },
     sh = { "shfmt" },
     css = { "prettierd" },
